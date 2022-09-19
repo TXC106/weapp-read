396ffa071489aed3c4a3203fa650deeaa60d36d2
Jane <Jane694194716@gmail.com>
2022-09-19 18:15:53 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  /*页面背景样式*/
+  page{
+    height: 100%;
+    background: #F0F0F0;
+  }
+  /*提示消息*/
+  .prompt{
+    margin-top: 50px;
+    margin-bottom: 30px;
+    font-size: 14px;
+    color: #888888;
+    text-align: center;
+  }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span class="msg">Hello,world!</span>
+    <van-button type="primary" @click="jump">按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    jump () {
+      this.$router.push('/pages/index/main')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.msg{
+  color: lightblue;
+}
+</style>

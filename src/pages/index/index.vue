82044eae5ea881953593d28b4b1901bd23d48668
Jane <Jane694194716@gmail.com>
2022-09-28 16:42:31 +0800

    初始测试代码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
     <span class="msg">Hello,world!</span>
+
     <van-button type="primary" @click="jump">按钮</van-button>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    jump () {
-      this.$router.push('/pages/index/main')
+  import { post } from '@/utils/request.js'
+
+  export default {
+    methods: {
+      jump() {
+        // this.$router.push('/pages/index/main')
+        post('https://www.baidu.com').then(response => {
+          console.log('getresponse!')
+        })
+      }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
